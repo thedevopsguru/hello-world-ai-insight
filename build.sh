3833a47804f020302809ffea264aa6097a458bbf
thedevopsguru <41733996+thedevopsguru@users.noreply.github.com>
2021-09-15 15:00:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DOCKER_REPO=`aws ecr describe-repositories --repository-names ai-insight-demo | grep repositoryUri | cut -d "\"" -f 4`
-Docker build --no-cache -t ${DOCKER_REPO}:<docker_tag> .
-Docker push ${DOCKER_REPO}:<docker_tag>
+docker build --no-cache -t ${DOCKER_REPO}:1.0 .
+docker push ${DOCKER_REPO}:1.0
