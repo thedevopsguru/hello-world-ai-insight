ff662cfac1d3b5f32599143449ff4fc586e89790
thedevopsguru <41733996+thedevopsguru@users.noreply.github.com>
2021-09-15 16:28:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,9 @@
 
 dockerRepo=`aws ecr describe-repositories --repository-name ai-insight-demo --region ap-south-1 | grep repositoryUri | cut -d "\"" -f 4`
 dockerTag=`aws ecr list-images --repository-name ai-insight-demo --region ap-south-1 | grep imageTag | head -n 1 | cut -d "\"" -f 4`
-sed -e "s;DOCKER_IMAGE_NAME;${repositoryURI};g" scripts/template.json > taskDefinition.json
+sed -e "s;DOCKER_IMAGE_NAME;${repositoryURI};g" template.json > taskDefinition.json
+echo "I am here"
+echo $DOCKER_IMAGE_NAME
 aws ecs register-task-definition --family ai-insight-jenkins --cli-input-json file://taskDefinition.json --region ap-south-1
 revision=`aws ecs describe-task-definition --task-definition ai-insight-jenkins --region ap-south-1 | grep "revision" | tr -s " " | cut -d " " -f 3`
 aws ecs update-service --cluster ai-insight-cluster --service ai-insight-service --task-definition ai-insight-jenkins:${revision} --desired-count 1
